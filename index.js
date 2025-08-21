badcacc121c67dd78f6e371482b1705bf00a85b6
Nsan Samuel <nsansamuelpeniel237@gmail.com>
2025-08-21 12:05:41 +0100

    Add

--- index.js
+++ index.js
@@ -16,8 +16,11 @@ else{
     console.log("Database connection failed");
 }
 //Define our user routes
-
 app.use('/api/user', userRoutes);
+//Middleware to parse JSON bodies
+app.use(express.json());
+//Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
 
 
 app.listen(port, () => {
